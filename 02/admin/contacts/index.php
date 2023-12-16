62c70961b359a42b47feda30a8ca51fec2d14aee
Kori McClure <35781456+ganmcc6nku@users.noreply.github.com>
2023-12-16 00:22:56 -0500

    Kori's contributions
    not many and they dont work, and i don't care anymore

--- 02/admin/contacts/index.php
+++ 02/admin/contacts/index.php
@@ -1,47 +1,293 @@
 <?php
 
-require __DIR__ . '/db.php';
-require __DIR__ . '/team.php';
+include_once 'db.php';
+include_once 'contacts.php';
 
+$contactManager = new ContactManager($pdo);
+$contactRequests = $contactManager->index();
+//Include the contacts.php function
+//include('admin/contacts/contacts.php');
 
-$teamManager = new TeamManager($pdo);
-$teamMembers = $teamManager->getTeamMembers();
+//$contacts = getContacts();
 
-?>
+// Include the plain-text.php function
+//include('plain-text.php');
 
+// Call the function to get the content
+//$textContent = readTextFile(); 
+
+// Display the content
+echo <<<'HTML'
 <!DOCTYPE html>
 <html lang="en">
 
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Member Index</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <meta charset="utf-8" />
+    <title>Hiric</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Premium Bootstrap 5 Landing Page Template" />
+    <meta name="keywords" content="bootstrap 5, premium, marketing, multipurpose" />
+    <meta content="Themesbrand" name="author" />
+    <!-- favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+
+    <!-- css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- icon -->
+    <link href="css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="css/pe-icon-7-stroke.css" />
+
+    <link href="css/style.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/colors/cyan.css" id="color-opt">
+
 </head>
+<body data-bs-theme="light">
 
-<body>
-    <form method="post" action="../team/detail.php">
+    <!-- STRAT NAVBAR -->
+    <nav class="navbar navbar-expand-lg fixed-top navbar-white navbar-custom sticky sticky-white" role="navigation"
+        id="navbar">
+        <div class="container">
+            <!-- LOGO -->
+            <a class="navbar-brand logo text-uppercase" href="index.html">
+                <i class="mdi mdi-alien"></i>Hiric
+            </a>
 
-        <?php foreach ($teamMembers as $teamMember): ?>
-            <button class="btn btn-primary" type="submit" name="team_member_index" value="<?php echo $teamMember['id']; ?>">
-                <?php echo htmlspecialchars($teamMember['Team member']); ?>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="mdi mdi-menu text-dark"></i>
             </button>
-        <?php endforeach; ?>
 
-        <input type="hidden" name="selected_team_member_index" id="selected_team_member_index" value="">
-    </form>
-    
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav navbar-center" id="navbar-navlist">
+                    <li class="nav-item">
+                        <a data-scroll href="#home" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a data-scroll href="#features" class="nav-link">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a data-scroll href="#services" class="nav-link">Products & Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a data-scroll href="#about" class="nav-link">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a data-scroll href="#pricing" class="nav-link">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a data-scroll href="#blog" class="nav-link">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a data-scroll href="#contact" class="nav-link">Contact</a>
+                    </li>
 
-    <script>
-        const buttons = document.querySelectorAll('button[name="team_member_index"]');
-        const selectedTeamMemberIndexInput = document.getElementById('selected_team_member_index');
-        buttons.forEach((button) => {
-            button.addEventListener('click', () => {
-                selectedTeamMemberIndexInput.value = button.value;
-            });
-        });
-    </script>
+                </ul>
+                <div class="nav-button ms-auto">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <button type="button"
+                                class="btn btn-primary navbar-btn btn-rounded waves-effect waves-light">Try it
+                                Free</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- END NAVBAR -->
 
+    <!--START HOME-->
+    <section class="section bg-home home-half" id="home" data-image-src="images/bg-home.jpg">
+        <div class="bg-overlay"></div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 text-white text-center">
+                    <h4 class="home-small-title">Awesome Design</h4>
+                    <h1>Contact Requests</h1>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+HTML;
+         foreach ($contactRequests as $key => $request): 
+echo <<<'HTML'
+            <tr>
+
+                <td>td testing</td>
+                <td>
+HTML;
+				 echo $request['id']; 
+echo <<<'HTML'
+				</td>
+                <td>
+HTML;
+				 echo $request['name'];
+echo <<<'HTML'
+				</td>
+                <td>
+HTML;
+				 echo $request['email']; 
+echo <<<'HTML'
+				</td>
+
+                <td>
+                    <a href="detail.php?id=<?php echo $request['id']; ?>">View Details</a>
+                </td>
+                
+            </tr>
+HTML;
+         endforeach;
+echo <<<'HTML'
+</table>           
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+ <!--END HOME-->
+ 
+ <!--START FOOTER-->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 mt-4">
+                    <a class="footer-logo text-uppercase" href="#">
+                        <i class="mdi mdi-alien"></i>Hiric
+                    </a>
+                    <div class="text-muted mt-4">
+                        <ul class="list-unstyled footer-list">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 mt-4">
+                    <h4>Information</h4>
+                    <div class="text-muted mt-4">
+                        <ul class="list-unstyled footer-list">
+                            <li><a href="#">Terms & Condition</a></li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Bookmarks</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 mt-4">
+                    <h4>Support</h4>
+                    <div class="text-muted mt-4">
+                        <ul class="list-unstyled footer-list">
+                            <li><a href="">FAQ</a></li>
+                            <li><a href="">Contact</a></li>
+                            <li><a href="">Disscusion</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 mt-4">
+                    <h4>Subscribe</h4>
+                    <div class="mt-4">
+                        <p>In an ideal world this text wouldn’t exist, a client would acknowledge the importance of
+                            having web copy before the design starts.</p>
+                    </div>
+                    <form class="form subscribe">
+                        <input placeholder="Email" class="form-control text-white" required>
+                        <a href="#" class="submit"><i class="pe-7s-paper-plane"></i></a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--END FOOTER-->
+
+    <!--START FOOTER ALTER-->
+    <div class="footer-alt">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="float-sm-start pull-none">
+                        <p class="copy-rights  mb-3 mb-sm-0">
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script> © Hiric - Themesbrand
+                        </p>
+                    </div>
+                    <div class="float-sm-end pull-none copyright">
+                        <ul class="list-inline d-flex flex-wrap social m-0">
+                            <li class="list-inline-item"><a href="" class="social-icon"><i
+                                        class="mdi mdi-facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="" class="social-icon"><i
+                                        class="mdi mdi-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="" class="social-icon"><i
+                                        class="mdi mdi-linkedin"></i></a></li>
+                            <li class="list-inline-item"><a href="" class="social-icon"><i
+                                        class="mdi mdi-google-plus"></i></a></li>
+                            <li class="list-inline-item"><a href="" class="social-icon"><i
+                                        class="mdi mdi-microsoft-xbox"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- <div class="clearfix"></div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--END FOOTER ALTER-->
+
+    <!-- Style switcher -->
+    <div id="style-switcher">
+        <div>
+            <h3 class="">Select your color</h3>
+            <ul class="pattern">
+                <li>
+                    <a class="color1" href="javascript: void(0);" onclick="setColor('cyan')"></a>
+                </li>
+                <li>
+                    <a class="color2" href="javascript: void(0);" onclick="setColor('red')"></a>
+                </li>
+                <li>
+                    <a class="color3" href="javascript: void(0);" onclick="setColor('green')"></a>
+                </li>
+                <li>
+                    <a class="color4" href="javascript: void(0);" onclick="setColor('pink')"></a>
+                </li>
+                <li>
+                    <a class="color5" href="javascript: void(0);" onclick="setColor('blue')"></a>
+                </li>
+                <li>
+                    <a class="color6" href="javascript: void(0);" onclick="setColor('purple')"></a>
+                </li>
+                <li>
+                    <a class="color7" href="javascript: void(0);" onclick="setColor('orange')"></a>
+                </li>
+                <li>
+                    <a class="color8" href="javascript: void(0);" onclick="setColor('yellow')"></a>
+                </li>
+            </ul>
+        </div>
+        <div class="bottom">
+            <a href="javascript: void(0);" id="mode" class="mode-btn text-white">
+                <i class="mdi mdi-weather-sunny bx-spin mode-light"></i>
+                <i class="mdi mdi-moon-waning-crescent mode-dark"></i>
+            </a>
+            <a href="javascript: void(0);" class="settings" onclick="toggleSwitcher()"><i
+                    class="mdi mdi-cog  mdi-spin"></i></a>
+        </div>
+    </div>
+    <!-- end Style switcher -->
+
+    <!-- javascript -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/smooth-scroll.polyfills.min.js"></script>
+    <script src="js/gumshoe.polyfills.min.js"></script>
+    <!-- Main Js -->
+    <script src="js/app.js"></script>
 </body>
-
 </html>
+HTML;?>
