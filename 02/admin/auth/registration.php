be75afa931e546936e27af7c449d0170a7ba529d
Alli <alligarcia2021@gmail.com>
2023-12-16 06:29:33 -0500

    fixing paths

--- 02/admin/auth/registration.php
+++ 02/admin/auth/registration.php
@@ -56,16 +56,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <a data-scroll href="#home" class="nav-link">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a data-scroll href="../../auth/registration.php" class="nav-link">Awards</a>
+                        <a data-scroll href="../../admin/auth/registration.php" class="nav-link">Awards</a>
                     </li>
                     <li class="nav-item">
-                        <a data-scroll href="../../auth/registration.php" class="nav-link">Listings</a>
+                        <a data-scroll href="../../admin/auth/registration.php" class="nav-link">Listings</a>
                     </li>
                     <li class="nav-item">
-                        <a data-scroll href="../../auth/registration.php" class="nav-link">Teams</a>
+                        <a data-scroll href="../../admin/auth/registration.php" class="nav-link">Teams</a>
                     </li>                    
                     <li class="nav-item">
-                        <a data-scroll href="../../auth/registration.php" class="nav-link">Contact</a>
+                        <a data-scroll href="../../admin/auth/registration.php" class="nav-link">Contact</a>
                     </li>
                     <li class="nav-item">
                         <a data-scroll href="../../admin/auth/login.php" class="nav-link">Login</a>
